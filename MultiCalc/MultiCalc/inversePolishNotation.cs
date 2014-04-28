bc6d90611597ca8fb54df63dbc0e490342339925
StoltZ <troels.hagensen@gmail.com>
2014-04-28 16:43:59 +0200

    StartUp
    Mads bugs

---
+++ MultiCalc/MultiCalc/inversePolishNotation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultiCalc
+{
+	public partial class inversePolishNotation : Gtk.Window
+	{
+		public inversePolishNotation () : 
+			base (Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+
+		protected void OnButton0Clicked (object sender, EventArgs e)
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
+
