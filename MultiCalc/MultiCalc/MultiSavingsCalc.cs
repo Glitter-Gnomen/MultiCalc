e1b8c2639c3461de612ed08ecba2be6971bac9df
milkymads <mads.joens@gmail.com>
2014-04-29 11:17:56 +0200

    Started writing comments on other calculators

--- MultiCalc/MultiCalc/MultiSavingsCalc.cs
+++ MultiCalc/MultiCalc/MultiSavingsCalc.cs
@@ -13,8 +13,11 @@ namespace MultiCalc
 
 		protected void OnButton1Clicked (object sender, EventArgs e)
 		{
+			//The needed variables are declared - all of them as doubles as they are to
+			//Be used with Math.Pow
 			double deposit, rate, terms, FTV;
 
+			//These lines makes it possible for the 
 			deposit = Convert.ToDouble (num1.Text);
 			rate = Convert.ToDouble (num2.Text);
 			terms = Convert.ToDouble (num3.Text);
