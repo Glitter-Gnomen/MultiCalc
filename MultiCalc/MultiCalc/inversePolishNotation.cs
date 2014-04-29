d6b69f5def39ebaa5f13ffe174b6759e99c13e6e
StoltZ <troels.hagensen@gmail.com>
2014-04-29 13:58:50 +0200

    Comments added
    A few comments added

--- MultiCalc/MultiCalc/inversePolishNotation.cs
+++ MultiCalc/MultiCalc/inversePolishNotation.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿//Inverse Polish Notation style calculator 
+//v1.2 with GUI in Gtk#
+
+using System;
 
 namespace MultiCalc
 {
+	//Gtk# Window Class
 	public partial class inversePolishNotation : Gtk.Window
 	{
+		//Declaration of the variables used througout the program
 		double var1, var2, result;
 		public inversePolishNotation () : 
 			base (Gtk.WindowType.Toplevel)
@@ -11,15 +16,20 @@ namespace MultiCalc
 			this.Build ();
 		}
 
-
+		//Addition operation
 		protected void OnButtonPlusClicked (object sender, EventArgs e)
 		{
+			//Instatiation of variables through a user input
+			//The input is is regarded as a string, through typecasting this is converted to type double
 			var1 = Convert.ToDouble (entry1.Text);
 			var2 = Convert.ToDouble (entry2.Text);
+			//Calculation of the result
 			result = var1 + var2;
+			//The result is converted to a string and parsed to a entry field
 			entry3.Text = result.ToString ();
-		}
 
+		}
+		//Modulus operation
 		protected void OnButtonModulusClicked (object sender, EventArgs e)
 		{
 			var1 = Convert.ToDouble (entry1.Text);
@@ -27,7 +37,7 @@ namespace MultiCalc
 			result = var1 % var2;
 			entry3.Text = result.ToString ();
 		}
-
+		//Division operation
 		protected void OnButtonDivideClicked (object sender, EventArgs e)
 		{
 			var1 = Convert.ToDouble (entry1.Text);
@@ -35,7 +45,7 @@ namespace MultiCalc
 			result = var1 / var2;
 			entry3.Text = result.ToString ();
 		}
-
+		//Multiplying operation
 		protected void OnButtonMultiplyClicked (object sender, EventArgs e)
 		{
 			var1 = Convert.ToDouble (entry1.Text);
@@ -43,7 +53,7 @@ namespace MultiCalc
 			result = var1 * var2;
 			entry3.Text = result.ToString ();
 		}
-
+		//Subtraction operation
 		protected void OnButtonMinusClicked (object sender, EventArgs e)
 		{
 			var1 = Convert.ToDouble (entry1.Text);
@@ -53,4 +63,3 @@ namespace MultiCalc
 		}
 	}
 }
-
