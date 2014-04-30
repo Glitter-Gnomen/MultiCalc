ca7e356d10a41b162fa1407ccc651b42a2dc3456
Rasmus Kure <rasmus_kure@hotmail.com>
2014-04-30 12:44:01 +0200

    Update SpeedConvWindow.cs

--- MultiCalc/MultiCalc/SpeedConvWindow.cs
+++ MultiCalc/MultiCalc/SpeedConvWindow.cs
@@ -14,32 +14,40 @@ namespace MultiCalc
 		protected void OnButtonCalc1Clicked (object sender, EventArgs e)
 		{
 			//Kilometers per hour to miles per hour
+			//Creates two variables of the type double
 			double kph, mph;
 
 			kph = Convert.ToDouble (entryKPH.Text);
+			//Formula that convert kph to mph
 			mph = kph / 1.6093;
 
+			//Makes the number into a string and store it in the label
 			label21.Text =  mph.ToString ();
 		}
 
 		protected void OnButtonCalc2Clicked (object sender, EventArgs e)
 		{
 			//Miles per hour to kilometers per hour
+			//Creates two variables of the type double
 			double kph2, mph2;
 
 			mph2 = Convert.ToDouble (entryMPH.Text);
+			//Formula that convert mph to kph
 			kph2 = mph2 * 1.6093;
 
+			//Makes the number into a string and store it in the label
 			label22.Text =  kph2.ToString ();		
 		}
 
 		protected void OnButtonClear1Clicked (object sender, EventArgs e)
 		{
+			//Clear the old data from the fields
 			entryKPH.Text = label21.Text = "";
 		}
 
 		protected void OnButtonClear2Clicked (object sender, EventArgs e)
 		{
+			//Clear the old data from the fields
 			entryMPH.Text = label22.Text = "";
 
 		}
