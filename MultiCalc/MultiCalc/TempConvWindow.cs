38ff6c5444e56c8d220dbf9eb2fc458195816d18
Rasmus Kure <rasmus_kure@hotmail.com>
2014-04-30 12:41:36 +0200

    Update TempConvWindow.cs

--- MultiCalc/MultiCalc/TempConvWindow.cs
+++ MultiCalc/MultiCalc/TempConvWindow.cs
@@ -14,9 +14,11 @@ namespace MultiCalc
 		protected void OnButtonCalc1Clicked (object sender, EventArgs e)
 		{
 			//Celsius to Fahrenheit
+			//Created two variable of the type double
 			double cel, fah;
 
 			cel = Convert.ToDouble (entryCelsius.Text);
+			//Formula that converts celsius to fahrenheit
 			fah = cel * 9 / 5 + 32;
 
 			labelFahrenheit.Text =  fah.ToString ();
@@ -25,9 +27,11 @@ namespace MultiCalc
 		protected void OnButtonCalc2Clicked (object sender, EventArgs e)
 		{
 			//Fahrenheit to Celsius
+			//Created two variable of the type double
 			double cel2, fah2;
 
 			fah2 = Convert.ToDouble (entryFahrenheit.Text);
+			//Formula that converts fahrenheit to celsius
 			cel2 = (fah2 - 32) * 5 / 9;
 
 			labelCelsius.Text =  cel2.ToString ();
@@ -35,11 +39,13 @@ namespace MultiCalc
 
 		protected void OnButtonClear1Clicked (object sender, EventArgs e)
 		{
+			//Clear the fields for old data
 			labelFahrenheit.Text = entryCelsius.Text = "";
 		}
 
 		protected void OnButtonClear2Clicked (object sender, EventArgs e)
 		{
+			//Clear the fields for old data
 			labelCelsius.Text = entryFahrenheit.Text = "";		}
 	}
 }
